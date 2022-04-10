85f5b38986f02ba0429046bb22f83e1dfa83d22f
Denis Sigida <47885010+denis-sigida@users.noreply.github.com>
2022-04-10 17:33:13 +0300

    add tesst and create class intersaction

---
+++ IntersactionPoint.Test/ResultTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using IntersactionPont.MathLogic;
+using System.Numerics;
+
+namespace IntersactionPoint.Test
+{
+    [TestClass]
+    public class ResultTest
+    {
+        [TestMethod]
+        public void CheckIntersactionPointValue()
+        {
+            Vector2 correct = new Vector2(4, 8);
+
+
+            Assert.AreEqual(correct, new Vector2(4, 8));
+        }
+    }
+}
---
+++ IntersactionPont.MathLogic/Model/Line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntersactionPont.MathLogic.Model
+{
+    public struct Line
+    {
+        public Vector2 Point1;
+        public Vector2 Point2;
+
+        public Line(Vector2 point1, Vector2 point2)
+        {
+            Point1=point1;
+            Point2=point2;
+        }
+    }
+}
